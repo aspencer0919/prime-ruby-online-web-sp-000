f0cbad1504009f839d6de650f4b0241243706f87
Adam Spencer <aspencer0919@gmail.com>
2020-04-06 15:13:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 num = []
 
 def prime?(n)
-  for d in 2..(n - 1)
-   if (n % d) == 0
+  if n < 1
     return false
-   end
+  else
+    for d in 2..(n - 1)
+      if (n % d) == 0
+        return false
+      end
+    end
+    true
   end
-  true
 end
